e18c0bc1ec05c7d9367c98af749b96f3fa9b0aec
HD-cjy <hdcjy5142@gmail.com>
2023-03-23 12:48:06 +0900

    worng_commentedit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import Library from './chapter_03/Library'; //index에 이거 추가
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library /> //index에 이거 변경 
+    <Library /> 
+    {/* index에 이거 변경  */}
   </React.StrictMode>
 );
 
