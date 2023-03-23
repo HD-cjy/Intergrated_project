1259f7563b4a07058efa885bee337137d8e24034
HD-cjy <hdcjy5142@gmail.com>
2023-03-23 12:45:15 +0900

    edit index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from './chapter_03/Library'; //index에 이거 추가 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library /> //index에 이거 변경 
   </React.StrictMode>
 );
 
