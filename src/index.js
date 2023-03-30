5c0a36370b186ac5302aba96c3ed381d95d9efea
HD-cjy <hdcjy5142@gmail.com>
2023-03-30 12:44:12 +0900

    readme0330

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library'; //index에 이거 추가 
+import Clock from './chapter_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library /> 
+    <Clock/>
     {/* index에 이거 변경  */}
   </React.StrictMode>
 );
