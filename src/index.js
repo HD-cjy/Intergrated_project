bdf548f58b334e776876e3383a71d45198811999
HD-cjy <hdcjy5142@gmail.com>
2023-05-01 13:20:10 +0900

    editall

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library'; //index에 이거 추가 
 import Clock from './chapter_04/Clock';
 import Comment from './chater_05/commemt';
+import ConfirmButton from './chapter_08/ConfirmButton';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Comment/>
+    <ConfirmButton/>
     {/* index에 이거 변경  */}
   </React.StrictMode>
 );
