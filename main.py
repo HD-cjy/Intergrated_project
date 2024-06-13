293a1caf016bf98237c4f2e97124b6b1a095dc63
hd-cjy <ccc5142@naver.com>
2024-06-14 00:49:17 +0900

    setting
    setting

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
+class Memo(BaseModel):
+    id:str
+    content:str
+
+memos = []
+
 app = FastAPI()
 
+
+@app.post("/memos")
+def create_memo(memo:Memo):
+    memos.append(memo)
+    return '메모추가에 성공했습니다.'
+    
+@app.get("/memos")
+def read_memo():
+    return memos
 app.mount("/", StaticFiles(directory='static',html=True),name='static')
