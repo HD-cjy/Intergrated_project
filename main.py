832c051cabf6b878d951e8b0863cb64134959a7a
hd-cjy <ccc5142@naver.com>
2024-06-14 03:08:55 +0900

    web_memo 입력, 수정까지 완료
    web_memo 입력, 수정까지 완료
    삭제기능 구현중 ..

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 class Memo(BaseModel):
-    id:str
-    content:str
+    id: str  # ID는 문자열로 정의
+    content: str  # content로 수정
 
 memos = []
 
 app = FastAPI()
 
-
 @app.post("/memos")
-def create_memo(memo:Memo):
+def create_memo(memo: Memo):
     memos.append(memo)
-    return '메모추가에 성공했습니다.'
-    
+    return {'message': '메모추가에 성공했습니다.'}
+
 @app.get("/memos")
 def read_memo():
     return memos
-app.mount("/", StaticFiles(directory='static',html=True),name='static')
+
+@app.put("/memo/{memo_id}")
+def put_memo(memo_id: str, req_memo: Memo):
+    for memo in memos:
+        if memo.id == memo_id:
+            memo.content = req_memo.content
+            return {'message': '성공했습니다.'}
+    return {'message': '그런 메모는 없습니다.'}
+
+app.mount("/", StaticFiles(directory='static', html=True), name='static')
--- static/app.js
+++ static/app.js
@@ -1,13 +1,38 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요!");
+  if (editInput === null) return; // 사용자가 취소 버튼을 누르면 아무것도 하지 않음.
+  const res = await fetch(`/memo/${id}`, {
+    method: "PUT", //값을 수정하는 method는 PUT이 좋음.
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
 function displayMemo(memo) {
   const ul = document.querySelector("#memo-ul");
+
   const li = document.createElement("li");
-  li.innerText = `[id: ${memo.id}] ${memo.content}`;
+  li.innerText = `[id:${memo.id}] ${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
   ul.appendChild(li);
+  li.appendChild(editBtn);
 }
+
 async function readMemo() {
   const res = await fetch("/memos");
-  const jsonRes = await res.json();
-  // jsonRes = [{id:123,content:'블라블라'}]
+  const jsonRes = await res.json(); // jsonRes = [{id:123,content:'블라블라'}]
   const ul = document.querySelector("#memo-ul");
   ul.innerHTML = "";
   jsonRes.forEach(displayMemo);
@@ -20,11 +45,10 @@ async function createMemo(value) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      id: new Date().getTime(),
+      id: new Date().getTime().toString(),
       content: value,
     }),
-  }); //post요청
-  //   const jsonRes = await res.json();
+  }); //post요청  //   const jsonRes = await res.json();
   readMemo();
 }
 
@@ -34,6 +58,7 @@ function handleSubmit(event) {
   createMemo(input.value);
   input.value = "";
 }
+
 const form = document.querySelector("#memo-form");
 form.addEventListener("submit", handleSubmit);
 
